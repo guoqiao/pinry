890ded4e15584ddb0d4ee63a24ec084865ecf35a
guoqiao <guoqiao@gmail.com>
2012-06-05 14:31:52 +0800

    add new_album.py

---
+++ pinry/pins/templatetags/new_album.py
@@ -0,0 +1,13 @@
+from django.template.loader import render_to_string
+from django.template import Library
+
+from pinry.pins.forms import AlbumForm
+
+
+register = Library()
+
+
+@register.simple_tag
+def new_album():
+    return render_to_string('pins/templatetags/new_album.html',
+        {'form': AlbumForm()})
