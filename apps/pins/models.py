d265bb9e983ec1047c171945dc9aa82d0d0f2e55
guoqiao <guoqiao@gmail.com>
2012-06-07 16:19:08 +0800

    change fields

--- apps/pins/models.py
+++ apps/pins/models.py
@@ -1,5 +1,6 @@
 import os
 from django.db import models
+from django.contrib.auth.models import User
 from django.conf import settings
 from django.core.files import File
 from django.core.files.temp import NamedTemporaryFile
@@ -10,27 +11,34 @@ import urllib2
 
 
 class Album(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, unique=True)
+    create = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User)
 
     def __unicode__(self):
         return self.name
 
     def path(self):
-        return os.path.join(settings.PINS_ROOT, self.name)
+        return os.path.join(settings.ALBUM_ROOT, str(self.id))
 
     def save(self, *args, **kwargs):
-        if not os.path.exists(self.path()):
-            os.mkdir(self.path())
         super(Album,self).save()
+        if not os.path.exists(self.path()):
+            os.makedirs(self.path())
+
+def pin_path(instance,filename):
+    return instance.path(filename)
 
 class Pin(models.Model):
-    #album = models.ForeignKey(Album)
-    #url = models.CharField(max_length=511)
-    image = ImageWithThumbsField(upload_to='pins/pin', sizes=((200, 1000),))
+    album = models.ForeignKey(Album)
+    image = ImageWithThumbsField(upload_to=pin_path, sizes=((200, 1000),))
     description = models.TextField(blank=True, null=True)
 
     def __unicode__(self):
         return self.image.name
+
+    def path(self,filename):
+        return os.path.join(self.album.path(),filename)
 
     def save(self, *args, **kwargs):
         '''
