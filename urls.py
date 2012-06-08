31778a0fe54a4c5485e7f70927f670f89df72e84
guoqiao <guoqiao@gmail.com>
2012-06-08 11:29:20 +0800

    mv api.urls to project urls

--- urls.py
+++ urls.py
@@ -2,9 +2,19 @@ from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
 from django.conf import settings
 
+from tastypie.api import Api
+from api.api import AlbumResource,PinResource
+
+v1_api = Api(api_name='v1')
+v1_api.register(AlbumResource())
+v1_api.register(PinResource())
+
+urlpatterns = patterns('',
+    url(r'', include(v1_api.urls)),
+)
 
 urlpatterns = patterns('',
     url(r'', include('core.urls', namespace='core')),
     url(r'^pins/', include('pins.urls', namespace='pins')),
-    url(r'^api/', include('api.urls', namespace='api')),
+    url(r'^api/', include(v1_api.urls)),
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
