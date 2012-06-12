1560f07cb92347df259287d0fcd10f38e96affc6
guoqiao <guoqiao@gmail.com>
2012-06-12 17:22:44 +0800

    fix url delete-album

--- apps/pins/urls.py
+++ apps/pins/urls.py
@@ -4,9 +4,9 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns('pins.views',
     url(r'^$', 'recent_albums', name='recent-albums'),#show recent albums and paged by year
     url(r'^new-album/$', 'new_album', name='new-album'),# create new album
-    #url(r'^delete-album/$', 'delete_album', name='delete-album'),# create new album
 
     url(r'^(?P<pk>\d+)/$', 'recent_pins', name='recent-pins'),
+    url(r'^(?P<pk>\d+)/delete/$', 'delete_album', name='delete-album'),# create new album
     url(r'^(?P<pk>\d+)/new-pin/$', 'new_pin', name='new-pin'),# add a new pin to current album
     url(r'^(?P<pk>\d+)/upload/$', 'create_pin', name='upload'),
     url(r'^delete-pin/(?P<pk>\d+)/$', 'delete_pin', name='delete-pin'),
