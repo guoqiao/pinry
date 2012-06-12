91794399dd3460537e07b9f24c21a3083aa83c3a
guoqiao <guoqiao@gmail.com>
2012-06-12 15:35:19 +0800

    new urls, no class based views

--- apps/pins/urls.py
+++ apps/pins/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls import patterns, url
-from .views import PinCreateView, PinDeleteView
+#from .views import PinCreateView, PinDeleteView
 
 urlpatterns = patterns('pins.views',
     url(r'^$', 'recent_albums', name='recent-albums'),#show recent albums and paged by year
     url(r'^new-album/$', 'new_album', name='new-album'),# create new album
+    #url(r'^delete-album/$', 'delete_album', name='delete-album'),# create new album
+
     url(r'^(?P<pk>\d+)/$', 'recent_pins', name='recent-pins'),
     url(r'^(?P<pk>\d+)/new-pin/$', 'new_pin', name='new-pin'),# add a new pin to current album
-    (r'^(?P<pk>\d+)/upload/$', PinCreateView.as_view(), {}, 'upload'),
-    (r'^/pin-delete/(?P<pk>\d+)$', PinDeleteView.as_view(), {}, 'pin-delete'),
-    #url(r'^/album-delete/(\d+)$', PinDeleteView.as_view(), name='album-delete'),
+    url(r'^(?P<pk>\d+)/upload/$', 'create_pin', name='upload'),
+    url(r'^delete-pin/(?P<pk>\d+)/$', 'delete_pin', name='delete-pin'),
 )
