467a03061b191614a70d0a1a880b2ccdc3530209
guoqiao <guoqiao@gmail.com>
2012-06-13 11:16:14 +0800

    delete dir while delete album

--- apps/pins/models.py
+++ apps/pins/models.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 import os
+import shutil
 from django.db import models
 from django.contrib.auth.models import User
-from django.core.files import File
-from django.core.files.temp import NamedTemporaryFile
+from django.conf import settings
 from thumbs import ImageWithThumbsField
+
+ALBUMS_DIR = 'albums'
 
 class Album(models.Model):
     name = models.CharField(u'名称', max_length=255, unique=True)
@@ -22,26 +24,30 @@ class Album(models.Model):
         return ('recent-pins', [self.id])
 
     def path(self):
-        return os.path.join('albums', str(self.id))
+        return os.path.join(settings.MEDIA_ROOT, ALBUMS_DIR, str(self.id))
 
     def save(self, *args, **kwargs):
         super(Album,self).save()
         if not os.path.exists(self.path()):
             os.makedirs(self.path())
 
-def pin_path(instance,filename):
-    return instance.path(filename)
+    def delete(self, *args, **kwargs):
+        path = self.path()
+        if os.path.exists(path):
+            shutil.rmtree(path)
+        super(Album,self).delete()
+
+def pin_upload_to(instance,filename):
+    # the disk path relative to media root
+    return os.path.join(ALBUMS_DIR,str(instance.album.id),filename)
 
 class Pin(models.Model):
     album = models.ForeignKey(Album)
-    file = ImageWithThumbsField('照片',upload_to=pin_path, sizes=((200, 1000),))
+    file = ImageWithThumbsField('照片',upload_to=pin_upload_to, sizes=((200, 1000),))
     description = models.TextField('描述',blank=True, null=True)
 
     def __unicode__(self):
         return self.file.name
-
-    def path(self,filename):
-        return os.path.join(self.album.path(),filename)
 
     class Meta:
         ordering = ['-id']
