14b4eab65231baead426e0a7117058dc4ae37119
guoqiao <guoqiao@gmail.com>
2012-06-14 15:45:42 +0800

    change ip for product

--- settings/product.py
+++ settings/product.py
@@ -6,6 +6,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 MIDDLEWARE_CLASSES += ()   # Add extra classes here
 INSTALLED_APPS += ()       # Add extra apps here
 
-MEDIA_URL  = 'http://10.10.20.173/pinry_media/'
-STATIC_URL = 'http://10.10.20.173/pinry_static/'
+MEDIA_URL  = 'http://10.10.20.223/pinry_media/'
+STATIC_URL = 'http://10.10.20.223/pinry_static/'
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
