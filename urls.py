6f374141fb881ed9d6718cf1776867a16d6e225c
guoqiao <guoqiao@gmail.com>
2012-06-20 15:40:36 +0800

    enable admin

--- urls.py
+++ urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
 from django.conf import settings
+from django.contrib import admin
+admin.autodiscover()
 
 from tastypie.api import Api
 from api.api import AlbumResource,PinResource,UserResource
@@ -13,5 +15,6 @@ v1_api.register(PinResource())
 urlpatterns = patterns('',
     url(r'^albums/', include('pins.urls', namespace='pins')),
     url(r'^api/', include(v1_api.urls)),
+    url(r'^admin/', include(admin.site.urls)),
     url(r'', include('core.urls', namespace='core')),
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
