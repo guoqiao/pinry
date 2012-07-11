48efb4d954262c6d0aacc8da2d7834bde244caeb
guoqiao <guoqiao@gmail.com>
2012-07-11 14:47:47 +0800

    rm wrong comment and no use url

--- apps/pins/urls.py
+++ apps/pins/urls.py
@@ -2,13 +2,12 @@ from django.conf.urls import patterns, url
 #from .views import PinCreateView, PinDeleteView
 
 urlpatterns = patterns('pins.views',
-    url(r'^$', 'albums', name='albums'),#show recent albums and paged by year
-    url(r'^new-album/$', 'new_album', name='new-album'),# create new album
+    url(r'^$', 'albums', name='albums'),
+    url(r'^new-album/$', 'new_album', name='new-album'),
 
     url(r'^(?P<pk>\d+)/$', 'recent_pins', name='recent-pins'),
-    url(r'^(?P<pk>\d+)/delete/$', 'delete_album', name='delete-album'),# create new album
-    url(r'^(?P<pk>\d+)/download/$', 'download_album', name='download-album'),# create new album
-    #url(r'^(?P<pk>\d+)/new-pin/$', 'new_pin', name='new-pin'),# add a new pin to current album
+    url(r'^(?P<pk>\d+)/delete/$', 'delete_album', name='delete-album'),
+    url(r'^(?P<pk>\d+)/download/$', 'download_album', name='download-album'),
     url(r'^(?P<pk>\d+)/upload/$', 'upload_pin', name='upload-pin'),
 
     url(r'^delete-pin/(?P<pk>\d+)/$', 'delete_pin', name='delete-pin'),
